ef3e9d42a7424b09b42c251adce3ad21a901861c
Hoang Anh Tuan <tuan.hoang@sunteco.io>
2021-12-02 23:54:25 +0700

    Update arrow position

--- panorama.js
+++ panorama.js
@@ -4,9 +4,23 @@ function addPanorama() {
     dayLuiBaoLucXaPano.addEventListener('progress', onProgress);
     dayLuiBaoLucXaPano.addEventListener('enter', onEnter);
 
+    var lookAtPositions = [
+        new THREE.Vector3(5000, -2000, 0),
+        new THREE.Vector3(5000, -300, 40),
+        new THREE.Vector3(5000, -400, 0),
+        new THREE.Vector3(4000, -600, 5000),
+        new THREE.Vector3(5000, -1000, -2000),
+    ];
     // Second panorama
     dayLuiBaoLucGanPano = new PANOLENS.ImagePanorama('asset/demo/day-lui-bao-luc-gan.jpg');
     dayLuiBaoLucGanPano.addEventListener('progress', onProgress);
+    // dayLuiBaoLucGanPano.addEventListener('progress', function(e) {
+    //     onProgress(e);
+    //     tocamInfospot.focus();
+    // });
+    dayLuiBaoLucGanPano.addEventListener('enter-fade-start', function () {
+        viewer.tweenControlCenter(lookAtPositions[0], 0);
+    });
     dayLuiBaoLucGanPano.addEventListener('enter', onEnter);
 
     // Third panorama
@@ -29,17 +43,26 @@ function addPanorama() {
     phudieu1Pano = new PANOLENS.ImagePanorama('asset/demo/phu-dieu-1.jpg');
     phudieu1Pano.addEventListener('progress', onProgress);
     phudieu1Pano.addEventListener('enter', onEnter);
+    phudieu1Pano.addEventListener('enter-fade-start', function () {
+        viewer.tweenControlCenter(lookAtPositions[1], 0);
+    });
 
     phudieu2Pano = new PANOLENS.ImagePanorama('asset/demo/phu-dieu-2.jpg');
     phudieu2Pano.addEventListener('progress', onProgress);
     phudieu2Pano.addEventListener('enter', onEnter);
+    phudieu2Pano.addEventListener('enter-fade-start', function () {
+        viewer.tweenControlCenter(lookAtPositions[4], 0);
+    });
 
     letan1Pano = new PANOLENS.ImagePanorama('asset/demo/le-tan-1.jpg');
     letan1Pano.addEventListener('progress', function(e) {
         onProgress1(e);
         customInfospot.focus();
     });
-    letan1Pano.addEventListener('enter', onEnter);
+    letan1Pano.addEventListener('enter', function(e) {
+        onEnter();
+        // zoomIn();
+    });
 
     letan2Pano = new PANOLENS.ImagePanorama('asset/demo/le-tan-2.jpg');
     letan2Pano.addEventListener('progress', onProgress);
@@ -49,50 +72,69 @@ function addPanorama() {
     letan3Pano.addEventListener('progress', onProgress);
     letan3Pano.addEventListener('enter', onEnter);
 
-    cungLenTiengXaPano.addEventListener("click", function (e) {
+    phudieu2Pano.addEventListener("click", function (e) {
         if (e.intersects.length > 0) return;
         const a = viewer.raycaster.intersectObject(viewer.panorama, true)[0].point;
         console.log('click panorama\n', e, 'point\n', a);
     });
 
     // Linking panoramas
-    letan1Pano.link(letan2Pano, new THREE.Vector3(5000, -3000, -2000), 600, Image.ArrowLeft, "Bấm vào đây để sang khu lễ tân", 60);
-    letan1Pano.link(letan3Pano, new THREE.Vector3(5000, -3000, 2000), 600, Image.ArrowRight);
-    letan1Pano.link(dayLuiBaoLucXaPano, new THREE.Vector3(5000, -3000, 0), 600, Image.ArrowUp);
+    letan1Pano.link(letan2Pano, new THREE.Vector3(5000, -3000, -2000), 400, Image.ArrowLeft, "Bấm vào đây để sang khu lễ tân", 60);
+    letan1Pano.link(letan3Pano, new THREE.Vector3(5000, -3000, 2000), 400, Image.ArrowRight);
+    letan1Pano.link(dayLuiBaoLucXaPano, new THREE.Vector3(5000, -3000, 0), 400, Image.ArrowUp);
 
-    letan2Pano.link(letan1Pano, new THREE.Vector3(5000, -3000, 0), 600, Image.ArrowDown);
-    letan3Pano.link(letan1Pano, new THREE.Vector3(5000, -3000, 0), 600, Image.ArrowDown);
+    letan2Pano.link(letan1Pano, new THREE.Vector3(5000, -3000, 0), 400, Image.ArrowDown);
+    letan3Pano.link(letan1Pano, new THREE.Vector3(5000, -3000, 0), 400, Image.ArrowDown);
 
-    dayLuiBaoLucXaPano.link(dayLuiBaoLucGanPano, new THREE.Vector3(5000, -3000, 0), 600, Image.ArrowUp);
-    dayLuiBaoLucXaPano.link(anSinhPano, new THREE.Vector3(5000, -3000, -2000), 600, Image.ArrowLeft);
-    dayLuiBaoLucXaPano.link(koAiBiBoLaiPano, new THREE.Vector3(5000, -3000, 2000), 600, Image.ArrowRight);
-    dayLuiBaoLucXaPano.link(letan1Pano, new THREE.Vector3(5000, -4000, 0), 600, Image.ArrowDown);
+    dayLuiBaoLucXaPano.link(dayLuiBaoLucGanPano, new THREE.Vector3(5000, -3000, 0), 400, Image.ArrowUp);
+    dayLuiBaoLucXaPano.link(anSinhPano, new THREE.Vector3(5000, -2500, -3000), 400, Image.ArrowLeft);
+    dayLuiBaoLucXaPano.link(koAiBiBoLaiPano, new THREE.Vector3(5000, -2500, 3000), 400, Image.ArrowRight);
+    dayLuiBaoLucXaPano.link(letan1Pano, new THREE.Vector3(5000, -4000, 0), 400, Image.ArrowDown);
 
-    dayLuiBaoLucGanPano.link(dayLuiBaoLucXaPano, new THREE.Vector3(5000, -4000, 0), 600, Image.ArrowDown);
-    dayLuiBaoLucGanPano.link(cungLenTiengXaPano, new THREE.Vector3(5000, -300, 0), 600, Image.ArrowUp);
-    dayLuiBaoLucGanPano.link(phudieu1Pano, new THREE.Vector3(5000, -500, -5000), 600, Image.ArrowLeft);
-    dayLuiBaoLucGanPano.link(phudieu2Pano, new THREE.Vector3(5000, -500, 5000), 600, Image.ArrowRight);
+    dayLuiBaoLucGanPano.link(dayLuiBaoLucXaPano, new THREE.Vector3(4000, -5000, 0), 400, Image.ArrowDown);
+    dayLuiBaoLucGanPano.link(cungLenTiengXaPano, new THREE.Vector3(5000, -4000, 0), 400, Image.ArrowUp);
+    dayLuiBaoLucGanPano.link(phudieu1Pano, new THREE.Vector3(5000, -3500, -4500), 400, Image.ArrowLeft);
+    dayLuiBaoLucGanPano.link(phudieu2Pano, new THREE.Vector3(5000, -3500, 4500), 400, Image.ArrowRight);
 
-    anSinhPano.link(dayLuiBaoLucXaPano, new THREE.Vector3(5000, -3000, -500), 600, Image.ArrowDown);
-    anSinhPano.link(cungLenTiengXaPano, new THREE.Vector3(5000, -3000, 2000), 600, Image.ArrowRight);
-    anSinhPano.link(koAiBiBoLaiPano, new THREE.Vector3(5000, -3000, -3000), 600, Image.ArrowLeft);
+    anSinhPano.link(dayLuiBaoLucXaPano, new THREE.Vector3(5000, -3000, -500), 400, Image.ArrowDown);
+    anSinhPano.link(phudieu1Pano, new THREE.Vector3(5000, -3000, 2000), 400, Image.ArrowRight);
+    anSinhPano.link(koAiBiBoLaiPano, new THREE.Vector3(5000, -3000, -3000), 400, Image.ArrowLeft);
 
-    koAiBiBoLaiPano.link(dayLuiBaoLucXaPano, new THREE.Vector3(5000, -3000, -500), 600, Image.ArrowDown);
-    koAiBiBoLaiPano.link(anSinhPano, new THREE.Vector3(5000, -3000, 2000), 600, Image.ArrowRight);
-    koAiBiBoLaiPano.link(cungLenTiengXaPano, new THREE.Vector3(5000, -3000, -3000), 600, Image.ArrowLeft);
+    koAiBiBoLaiPano.link(dayLuiBaoLucXaPano, new THREE.Vector3(5000, -3000, -500), 400, Image.ArrowDown);
+    koAiBiBoLaiPano.link(anSinhPano, new THREE.Vector3(5000, -3000, 2000), 400, Image.ArrowRight);
+    koAiBiBoLaiPano.link(phudieu2Pano, new THREE.Vector3(5000, -3000, -3000), 400, Image.ArrowLeft);
 
-    cungLenTiengXaPano.link(cungLenTiengGanPano, new THREE.Vector3(5000, -300, 0), 600, Image.ArrowUp);
-    cungLenTiengXaPano.link(dayLuiBaoLucGanPano, new THREE.Vector3(5000, -3000, 0), 600, Image.ArrowDown);
-    cungLenTiengXaPano.link(anSinhPano, new THREE.Vector3(5000, -3000, -2000), 600, Image.ArrowLeft);
-    cungLenTiengXaPano.link(koAiBiBoLaiPano, new THREE.Vector3(5000, -3000, 2000), 600, Image.ArrowRight);
+    // cungLenTiengXaPano.link(cungLenTiengGanPano, new THREE.Vector3(5000, -300, 0), 400, Image.ArrowUp);
+    cungLenTiengXaPano.link(dayLuiBaoLucGanPano, new THREE.Vector3(5000, -3000, 0), 400, Image.ArrowDown);
+    cungLenTiengXaPano.link(phudieu1Pano, new THREE.Vector3(5000, -3000, -2000), 400, Image.ArrowLeft);
+    cungLenTiengXaPano.link(phudieu2Pano, new THREE.Vector3(5000, -3000, 2000), 400, Image.ArrowRight);
+    // bantayInfoSpot = new PANOLENS.Infospot(400, Image.ArrowRight);
+    // bantayInfoSpot.position.set(4000, -3500, 5000);
+    // cungLenTiengXaPano.add(bantayInfoSpot);
+    // bantayInfoSpot.addEventListener("click", function (event) {
+    //     viewer.tweenControlCenter(lookAtPositions[3], 0);
+    // });
+    cungLenTiengXaPano.addEventListener('enter-fade-start', function () {
+        viewer.tweenControlCenter(lookAtPositions[2], 0);
+        setTimeout(function(){ 
+            // alert('hello');
+        }, 2000);
+        zoomIn();
+    });
 
-    cungLenTiengGanPano.link(cungLenTiengXaPano, new THREE.Vector3(5000, -3000, 0), 600, Image.ArrowDown);
+    cungLenTiengGanPano.link(cungLenTiengXaPano, new THREE.Vector3(5000, -3000, 0), 400, Image.ArrowDown);
 
-    phudieu1Pano.link(dayLuiBaoLucGanPano, new THREE.Vector3(5000, -3000, 0), 600, Image.ArrowDown);
-    phudieu2Pano.link(dayLuiBaoLucGanPano, new THREE.Vector3(5000, -3000, 0), 600, Image.ArrowDown);
+    phudieu1Pano.link(dayLuiBaoLucGanPano, new THREE.Vector3(5000, -3000, 0), 400, Image.ArrowDown);
+    phudieu1Pano.link(anSinhPano, new THREE.Vector3(5000, -3000, -3000), 400, Image.ArrowLeft);
+    phudieu1Pano.link(cungLenTiengXaPano, new THREE.Vector3(5000, -3000, 3000), 400, Image.ArrowRight);
+
+    phudieu2Pano.link(dayLuiBaoLucGanPano, new THREE.Vector3(5000, -3000, 0), 400, Image.ArrowDown);
+    phudieu2Pano.link(cungLenTiengXaPano, new THREE.Vector3(5000, -3000, -3000), 400, Image.ArrowLeft);
+    phudieu2Pano.link(koAiBiBoLaiPano, new THREE.Vector3(5000, -3000, 3000), 400, Image.ArrowRight);
 
     viewer = new PANOLENS.Viewer({"controlButtons":['video','fullscreen']});
     viewer.add(letan1Pano, letan2Pano, letan3Pano, dayLuiBaoLucXaPano, dayLuiBaoLucGanPano, anSinhPano, koAiBiBoLaiPano, cungLenTiengXaPano, cungLenTiengGanPano, phudieu1Pano, phudieu2Pano);
+    
 }
 
 function addCustomInfoSpot() {
@@ -186,6 +228,20 @@ function addCustomInfoSpot() {
 var i = 1;
 // myLoop();
 
+var initialZoomDepth = 80;
+function zoomIn() {
+    setTimeout(function () {
+        console.log(initialZoomDepth);
+        viewer.setCameraFov(initialZoomDepth);
+        // viewer.getCamera().fov = initialZoomDepth;
+        initialZoomDepth -= 3;
+        if (initialZoomDepth > 50) {
+            zoomIn();
+        }
+        
+    }, 50)
+}
+
 function myLoop() {         //  create a loop function
     setTimeout(function () {   //  call a 3s setTimeout when the loop is called
         i++;
@@ -241,7 +297,7 @@ function myLoop() {         //  create a loop function
             i = 0;
         }
         myLoop();
-    }, 300)
+    }, 500)
 }
 
 function scaleUpPanorama(parent, isUp) {
